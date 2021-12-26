768dad62ee21328fe8692266ff1a6e6e5fc2782a
Jiovanni R [Windows] <srr190802@gmail.com>
2021-12-25 20:31:54 -0600

    Sidenav y toolbar agregados

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon'
 
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
+  exports: [
+    // Todo lo que este siendo exportado en este modulo, sera parte del modulo que lo importe
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
   ]
 })
 export class MaterialModule { }
